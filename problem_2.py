f534b8b47f4c3267fb7602de5a471bfc72323de0
ojasvi.sharma <165778974+ojasvi1224@users.noreply.github.com>
2024-04-18 05:31:52 +0530

    Create problem_2.py

---
+++ problem_2.py
@@ -0,0 +1,14 @@
+def get_name():
+    Name = input('Your Good Name:')
+    return Name
+
+def get_tshirt(brand_name,Size):
+    name = get_name()
+    Available=["spykar" , "levis", "us polo", "adidas"]
+    size=["s" , "m" , "l" , "xl"]
+    if brand_name in Available and Size in size:
+        print("Hi", {name} , ",the brand and size you are looking for is available in our store.")
+    else:
+       print("Hi", {name} , ",Unfortunately the brand or size you are looking for is not available in our store.")
+
+get_tshirt("levis","l")
